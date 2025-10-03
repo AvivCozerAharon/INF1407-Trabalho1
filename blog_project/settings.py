c6a0d021e2c776c060262bd55b774a673efff9a9
AvivCozerAharon <80224310+AvivCozerAharon@users.noreply.github.com>
2025-10-03 11:08:13 -0300

    Redefinir senha

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -222,3 +222,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Redireciona para a página de adicionar notícias após o login
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -34,6 +34,10 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('noticia/<int:pk>/editar/', views.editar_noticia, name='editar_noticia'),
     path('noticia/<int:pk>/apagar/', views.apagar_noticia, name='apagar_noticia'),
+    path("password_reset/", auth_views.PasswordResetView.as_view(template_name="password_reset_form.html",email_template_name="password_reset_email.html",subject_template_name="password_reset_subject.txt"), name="password_reset"),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name="password_reset_confirm"),
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name="password_reset_complete"),
 
 ]
 
